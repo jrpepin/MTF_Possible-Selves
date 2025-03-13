285b9416355bfd908f05feefa8522e8ab7badad4
jrpepin <j.pepin@utoronto.ca>
2025-03-13 17:53:25 -0400

    race gender figs

--- scripts/PS_02_tables & figures.R
+++ scripts/PS_02_tables & figures.R
@@ -301,9 +301,13 @@ read_docx() %>%
 # Create Figure 1 --------------------------------------------------------------
 
 ## Average Predictions 
-pp_sp   <- ggaverage(polr1.sp, terms = c("year.c [all]"))
-pp_pa   <- ggaverage(polr1.pa, terms = c("year.c [all]"))
-pp_wk   <- ggaverage(polr1.wk, terms = c("year.c [all]"))
+pp_sp   <- predict_response(polr1.sp, terms = c("year.c [all]"))
+pp_pa   <- predict_response(polr1.pa, terms = c("year.c [all]"))
+pp_wk   <- predict_response(polr1.wk, terms = c("year.c [all]"))
+
+### These all do the same thing, I think -- check against Stata later
+#pp_sp   <- ggaverage(polr1.sp, terms = c("year.c [all]"))
+#pp_sp   <- predict_response(polr1.sp, terms = c("year.c [all]"), margin = "marginalmeans")
 
 ## Combine dfs
 pp_sp$cat    <- "Spouse" 
@@ -382,10 +386,152 @@ plot(p1)
 invisible(dev.off())
 
 
-## Stacked bar chart <- syntax as sample for Fig2
-pp_sp %>%
-  ggplot(aes(x = x, y = predicted, fill = response.level, 
+# Figure 2 ---------------------------------------------------------------------
+
+polr2.sp.sex  <- polr(gdsp ~ year.c * sex + I(year.c^2) + 
+                    race + momed + region + religion + famstru.d,
+                  data = data, weights = svyweight, Hess = T)
+
+polr2.pa.sex  <- polr(gdpa ~ year.c * sex + I(year.c^2) + 
+                        race + momed + region + religion + famstru.d,
+                      data = data, weights = svyweight, Hess = T)
+
+polr2.wk.sex  <- polr(gdwk ~ year.c * sex + I(year.c^2) + 
+                        race + momed + region + religion + famstru.d,
+                      data = data, weights = svyweight, Hess = T)
+
+## Average Predictions 
+
+pp_sp_sex   <- predict_response(polr2.sp.sex, terms = c("year.c [all]", "sex"))
+pp_pa_sex   <- predict_response(polr2.pa.sex, terms = c("year.c [all]", "sex"))
+pp_wk_sex   <- predict_response(polr2.wk.sex, terms = c("year.c [all]", "sex"))
+
+## Combine dfs
+pp_sp_sex$cat    <- "Spouse" 
+pp_pa_sex$cat    <- "Parent" 
+pp_wk_sex$cat    <- "Worker" 
+
+df_pp_sex <- rbind(pp_wk_sex, pp_sp_sex, pp_pa_sex)
+
+## Tidy variables
+df_pp_sex$response.level <- factor(df_pp_sex$response.level, 
+                               levels=c("Very good", 
+                                        "Good", 
+                                        "Fairly good", 
+                                        "Not so good", 
+                                        "Poor"))
+
+df_pp_sex$cat <- factor(df_pp_sex$cat, 
+                    levels=c("Spouse", 
+                             "Parent",
+                             "Worker"))
+
+lables_year <- c("1984", "1995", "2005", "2015", "2023")
+
+## Draw figure
+p2 <- df_pp_sex %>%
+  filter(response.level == "Very good") %>%
+  ggplot(aes(x = x, y = predicted, color = group, 
              ymin = conf.low, ymax = conf.high)) +
-  geom_bar(position="fill", stat="identity") +
-  theme_minimal() 
+  geom_line(linewidth = 1) +
+  #  geom_bar(position="fill", stat="identity") +
+  facet_grid(cols = vars(cat),
+             scales = "free") +
+  theme_minimal() +
+  theme(#legend.position = c(1, 0),
+    #legend.justification = c(1, 0),
+    legend.position  = "right",
+    legend.title     = element_blank(),
+    strip.text.x     = element_text(face = "bold"),
+    panel.grid.minor = element_blank(),
+    panel.spacing    = unit(1.1, "cm", data = NULL)) +
+  scale_y_continuous(breaks = c(0., .25, .5, .75), limits=c(0, .75), labels = scales::percent) +
+  scale_x_continuous(breaks=c(-12.5, -1.54, 8.45, 18.45, 26.45), labels = lables_year) +
+  labs( x        = " ", 
+        y        = " ",
+        title    = "% who think they would be a 'very good' ______")
+
+p2
+
+## Save Fig 2
+agg_tiff(filename = file.path(here(outDir, figDir), "fig2.tif"), 
+         width=8, height=6, units="in", res = 800, scaling = 1)
+
+plot(p2)
+invisible(dev.off())
+
+
+# Figure 3 ---------------------------------------------------------------------
+
+polr2.sp.race  <- polr(gdsp ~ year.c * race + I(year.c^2) + 
+                        sex + momed + region + religion + famstru.d,
+                      data = data, weights = svyweight, Hess = T)
+
+polr2.pa.race  <- polr(gdpa ~ year.c * race + I(year.c^2) + 
+                        sex + momed + region + religion + famstru.d,
+                      data = data, weights = svyweight, Hess = T)
+
+polr2.wk.race  <- polr(gdwk ~ year.c * race + I(year.c^2) + 
+                        sex + momed + region + religion + famstru.d,
+                      data = data, weights = svyweight, Hess = T)
+
+## Average Predictions 
+
+pp_sp_race   <- predict_response(polr2.sp.race, terms = c("year.c [all]", "race"))
+pp_pa_race   <- predict_response(polr2.pa.race, terms = c("year.c [all]", "race"))
+pp_wk_race   <- predict_response(polr2.wk.race, terms = c("year.c [all]", "race"))
+
+## Combine dfs
+pp_sp_race$cat    <- "Spouse" 
+pp_pa_race$cat    <- "Parent" 
+pp_wk_race$cat    <- "Worker" 
+
+df_pp_race <- rbind(pp_wk_race, pp_sp_race, pp_pa_race)
+
+## Tidy variables
+df_pp_race$response.level <- factor(df_pp_race$response.level, 
+                                   levels=c("Very good", 
+                                            "Good", 
+                                            "Fairly good", 
+                                            "Not so good", 
+                                            "Poor"))
+
+df_pp_race$cat <- factor(df_pp_race$cat, 
+                        levels=c("Spouse", 
+                                 "Parent",
+                                 "Worker"))
+
+lables_year <- c("1984", "1995", "2005", "2015", "2023")
+
+## Draw figure
+p3 <- df_pp_race %>%
+  filter(response.level == "Very good") %>%
+  ggplot(aes(x = x, y = predicted, color = group, 
+             ymin = conf.low, ymax = conf.high)) +
+  geom_line(linewidth = 1) +
+  #  geom_bar(position="fill", stat="identity") +
+  facet_grid(cols = vars(cat),
+             scales = "free") +
+  theme_minimal() +
+  theme(#legend.position = c(1, 0),
+    #legend.justification = c(1, 0),
+    legend.position  = "right",
+    legend.title     = element_blank(),
+    strip.text.x     = element_text(face = "bold"),
+    panel.grid.minor = element_blank(),
+    panel.spacing    = unit(1.1, "cm", data = NULL)) +
+  scale_y_continuous(breaks = c(0., .25, .5, .75), limits=c(0, .75), labels = scales::percent) +
+  scale_x_continuous(breaks=c(-12.5, -1.54, 8.45, 18.45, 26.45), labels = lables_year) +
+  labs( x        = " ", 
+        y        = " ",
+        title    = "% who think they would be a 'very good' ______")
+
+p3
+
+## Save Fig 3
+agg_tiff(filename = file.path(here(outDir, figDir), "fig3.tif"), 
+         width=8, height=6, units="in", res = 800, scaling = 1)
+
+plot(p3)
+invisible(dev.off())
 
